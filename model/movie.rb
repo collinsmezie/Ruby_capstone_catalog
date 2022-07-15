69e32d8b505931a6e1e74ee799dc14fcbcacc070
Evans Sitibekiso <jevacrism@gmail.com>
2022-07-15 23:33:32 +0200

    update movie.rb

--- model/movie.rb
+++ model/movie.rb
@@ -1,22 +1,22 @@
 require 'date'
 require_relative './item'
-class Music < Item
-  attr_accessor :on_spotify
+class Movie < Item
+  attr_accessor :silet
 
-  def initialize(on_spotify:)
+  def initialize(silet:)
     super(publish_date: Date.today.to_s)
-    @on_spotify = on_spotify
+    @silet = silet
   end
 
   def to_json(*_args)
     JSON.dump(
-      on_spotify: @on_spotify
+      silet: @silet
     )
   end
 
   private
 
   def can_be_archived?
-    return true if super && @on_spotify
+    return true if super && @silet
   end
 end
