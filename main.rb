a014d4979cfecc527b2ed90a5d8437a83a2aea89
lyamz <davidlyangenda@gmail.com>
2022-07-15 16:07:13 +0200

    modify main.rb file

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
-require './start'
+require './app'
+require './input'
+require './storage'
+require './options'
 
 def main
-  puts "My Catalog\n\n"
-  start = Start.new
-  start.run
+  app = App.new
+  app.populate_app
+  app.populate_author
+  storage = Storage.new
+  storage.prepare_storage
+  storage.load_data(app)
+  options(app)
+  puts 'Thank you for using our service :)'
 end
 
 main
